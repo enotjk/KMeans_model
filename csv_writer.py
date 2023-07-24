090230fa8e34719f233d0d10594eea44f328749d
enotjk <129334043+enotjk@users.noreply.github.com>
2023-07-25 01:29:43 +0300

    Add files via upload

---
+++ csv_writer.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import csv
+
+from KMeans import clusters
+
+cluster_dfs = []
+for i, points in clusters.items():
+    cluster_df = pd.DataFrame(points, columns=['X', 'Y'])
+    cluster_df['Cluster'] = f'Cluster_{i}'
+    cluster_dfs.append(cluster_df)
+cluster_df = pd.concat(cluster_dfs, ignore_index=True)
+print(cluster_df)
+
+cluster_df.to_csv('clusters.csv', index=False)
