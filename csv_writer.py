d8a67f670b101c58ac088bb1018636724ec0c8c6
enotjk <129334043+enotjk@users.noreply.github.com>
2023-07-25 01:58:46 +0300

    Update csv_writer.py

--- csv_writer.py
+++ csv_writer.py
@@ -6,7 +6,7 @@ from KMeans import clusters
 cluster_dfs = []
 for i, points in clusters.items():
     cluster_df = pd.DataFrame(points, columns=['X', 'Y'])
-    cluster_df['Cluster'] = f'Cluster_{i}'
+    cluster_df['Cluster'] = f'Cluster_{i+1}'
     cluster_dfs.append(cluster_df)
 cluster_df = pd.concat(cluster_dfs, ignore_index=True)
 print(cluster_df)
